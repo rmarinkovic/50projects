cbaa638920721dfa307026171723c8f92b84f335
Roko Marinkovic <roko.marinkovic@gmail.com>
2022-05-11 14:03:30 +0200

    Event KeyCodes project finished

---
+++ event_keycodes/script.js
@@ -0,0 +1,17 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+  <div class="key">
+            ${e.key === ' ' ? 'Space' : e.key}
+            <small>e.key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small style="top: -38px;">e.keyCode (deprecated)</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>e.code</small>
+        </div>`;
+});
